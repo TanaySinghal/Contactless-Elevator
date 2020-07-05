18ca1a6401153454110aecb108463b8f01fbac5c
Tanay Singhal <singhal.tanay@gmail.com>
2020-07-05 14:46:00 -0400

    Imported haptic module from Tactile Breakout, but removed some things we don't need

---
+++ Assets/Scripts/Haptics/SDKExamples/TPSFollow.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Ultrahaptics;
+
+// Translated from C++
+// https://developer.ultrahaptics.com/knowledgebase/time-point-streaming-api-tutorial/
+
+// Also help support gave me this resource:
+// https://github.com/ultraleap/ultraleap-labs/blob/130e56147c70eb1b41da58089e7b3a13390fdb6b/HapticTextures/Assets/HapticTextures/Scripts/HapticRunner
+// The above resource shows you how to handle multiple circles AND how to track hand position
+
+public class TPSFollow : MonoBehaviour
+{
+    [SerializeField, Range(0f, 1f)]
+    float intensity = 1f; // 0 to 1
+    [SerializeField, Range(1f, 500f)]
+    float frequency = 100f;
+    [SerializeField, Range(0.001f, 0.05f)]
+    float radius = 0.01f;
+
+    TimePointStreamingEmitter _emitter;
+    bool _firstTime = true;
+    double _startTime;
+
+    TactileCircle circle;
+
+    void Start()
+    {
+        // Initialize circle
+        circle = new TactileCircle(
+            new Ultrahaptics.Vector3(0.0f, 0.0f, 0.2f),
+            intensity,
+            frequency,
+            radius
+        );
+
+        // Initialize the emitter
+        _emitter = new TimePointStreamingEmitter();
+        _emitter.initialize();
+
+        // Set callback and start emitter
+        _emitter.setEmissionCallback(MyEmitterCallback, circle);
+        _emitter.start();
+    }
+
+    // Update on every frame
+    void Update()
+    {
+        // Set circle to location of object
+        circle.position = transform.position.ToUH();
+
+        // Apply any changes to sliders in update
+        circle.intensity = intensity;
+        circle.drawFrequency = frequency;
+        circle.radius = radius;
+
+        // Update callback with updated circle
+        _emitter.setEmissionCallback(MyEmitterCallback, circle);
+    }
+
+    // Ensure the emitter is stopped on exit
+    void OnDisable()
+    {
+        _emitter.stop();
+    }
+
+    // Ensure the emitter is immediately disposed when destroyed
+    void OnDestroy()
+    {
+        _emitter.Dispose();
+        _emitter = null;
+    }
+
+    void MyEmitterCallback(TimePointStreamingEmitter emitter, OutputInterval interval, TimePoint deadline, object userObj)
+    {
+        // Loop through the samples in this interval
+        foreach (var sample in interval)
+        {
+            if (_firstTime)
+            {
+                _startTime = sample.seconds();
+                _firstTime = false;
+            }
+            double t = sample.seconds() - _startTime;
+            Ultrahaptics.Vector3 position = circle.EvaluateAt(t);
+
+            // Set the position and intensity of the persistent control 
+            // point to that of the modulated wave at this point in time.
+            // Note: we use 0 because there is only one circle
+            sample.persistentControlPoint(0).setPosition(position);
+            sample.persistentControlPoint(0).setIntensity(circle.intensity);
+        }
+    }
+}
---
+++ Assets/Scripts/Haptics/TactileRunner.cs
@@ -0,0 +1,160 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Ultrahaptics;
+
+// See the following resource for a basic example of using TPS
+// https://github.com/ultraleap/ultraleap-labs/blob/130e56147c70eb1b41da58089e7b3a13390fdb6b/HapticTextures/Assets/HapticTextures/Scripts/HapticRunner
+
+/// <summary>
+/// A singleton that manages and renders all tactile shapes to the UltraHaptics device
+/// </summary>
+public class TactileRunner : MonoBehaviourSingleton<TactileRunner>
+{
+    TimePointStreamingEmitter _emitter;
+    bool _firstTime = true;
+    double _startTime;
+
+    // Dictionary from tactile shape data to its view
+    Dictionary<TactileShape, GameObject> tactileShapes;
+    readonly uint maxShapes = 4;
+
+    [SerializeField] bool displayTactileShapes = false;
+    [SerializeField] Color lowFrequencyColor;
+    [SerializeField] int lowFrequencyValue = 50;
+    [SerializeField] Color highFrequencyColor;
+    [SerializeField] int highFrequencyValue = 200;
+
+    // View prefabs
+    [SerializeField] CircleView circleViewPrefab;
+    [SerializeField] LineView lineViewPrefab;
+
+    void Awake()
+    {
+        // Initialize shape list
+        tactileShapes = new Dictionary<TactileShape, GameObject>();
+
+
+        // Initialize the emitter
+        _emitter = new TimePointStreamingEmitter();
+        _emitter.initialize();
+
+        uint sampleRate = _emitter.setMaximumControlPointCount(maxShapes);
+        Debug.Log("Tactile sample rate: " + sampleRate);
+
+        // Set callback and start emitter (no need to pass object)
+        _emitter.setEmissionCallback(MyEmitterCallback, null);
+        bool started = _emitter.start();
+
+        if (!started)
+        {
+            Debug.LogError("Could not start UltraHaptics TPS emitter. Connection status is " + _emitter.isConnected() + ".");
+        }
+    }
+
+    // Whenever an editor variable changes
+    void OnValidate() {
+        foreach (UnityEngine.Transform child in transform) {
+            child.gameObject.SetActive(displayTactileShapes);
+        }
+    }
+
+    // Ensure the emitter is stopped on exit
+    void OnDisable()
+    {
+        _emitter.stop();
+    }
+
+    // Ensure the emitter is immediately disposed when destroyed
+    void OnDestroy()
+    {
+        _emitter.Dispose();
+        _emitter = null;
+    }
+
+    public void AddShape(TactileShape shape)
+    {
+        if (!_emitter.isConnected())
+        {
+            Debug.LogWarning("Failed to add shape. Tactile emitter is disconnected.");
+            return;
+        }
+
+        if (tactileShapes.Count < maxShapes)
+        {
+            // Instantiate view and add it to shape
+            GameObject view = InstantiateViewFromShape(shape);
+            view.transform.SetParent(transform);
+            view.gameObject.SetActive(displayTactileShapes);
+            tactileShapes.Add(shape, view);
+        }
+        else
+        {
+            Debug.LogWarning("Failed to add shape. Maximum shapes allowed: " + maxShapes);
+        }
+    }
+
+    public bool ContainsShape(TactileShape shape)
+    {
+        return tactileShapes.ContainsKey(shape);
+    }
+
+    public void RemoveShape(TactileShape shape)
+    {
+        // Destroy the shape's view
+        GameObject view = tactileShapes[shape];
+        Destroy(view);
+
+        // Remove the shape
+        tactileShapes.Remove(shape);
+    }
+
+    public Color GetColorFromFrequency(float frequency) {
+        float t = Mathf.InverseLerp(lowFrequencyValue, highFrequencyValue, frequency);
+        return Color.Lerp(lowFrequencyColor, highFrequencyColor, t);
+    }
+
+    // Note: errors here may go silent. For some reason, we cannot reference transform here at all.
+    void MyEmitterCallback(TimePointStreamingEmitter emitter, OutputInterval interval, TimePoint deadline, object userObj)
+    {
+        // Loop through the samples in this interval
+        foreach (var sample in interval)
+        {
+            if (_firstTime)
+            {
+                _startTime = sample.seconds();
+                _firstTime = false;
+            }
+            double t = sample.seconds() - _startTime;
+
+            // Note: technically there is no guarantee that the tactile shapes will be in the right order
+            // but this should work for now...
+            int i = 0;
+            foreach (TactileShape shape in tactileShapes.Keys) {
+                // Set the position and intensity of the persistent control 
+                // point to that of the modulated wave at this point in time.
+                Ultrahaptics.Vector3 point = shape.EvaluateAt(t);
+                sample.persistentControlPoint(i).setPosition(point);
+                sample.persistentControlPoint(i).setIntensity(shape.intensity);
+                ++ i;
+            }
+        }
+    }
+
+    GameObject InstantiateViewFromShape(TactileShape shape)
+    {
+        switch (shape)
+        {
+            case TactileCircle circle:
+                CircleView circleView = Instantiate(circleViewPrefab);
+                circleView.tactileCircle = (TactileCircle)shape;
+                return circleView.gameObject;
+            case TactileLine line:
+                LineView lineView = Instantiate(lineViewPrefab);
+                lineView.tactileLine = (TactileLine)shape;
+                return lineView.gameObject;
+            default:
+                Debug.LogError("Shape of type " + shape.GetType() + " is not associated with a view");
+                return null;
+        }
+    }
+}
